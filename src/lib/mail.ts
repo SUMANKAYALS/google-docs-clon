b0ee897c7313eb488c925e69d22c65736e59774b
Suman Kayal <suman13kayal@gmail.com>
2026-07-27 01:36:06 +0530

    fix: otp problem 01

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -11,10 +11,20 @@ import nodemailer from "nodemailer";
 // });
 
 
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST || "smtp.gmail.com",
+//   port: Number(process.env.SMTP_PORT || "587"),
+//   secure: Number(process.env.SMTP_PORT) === 465,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || "smtp.gmail.com",
-  port: Number(process.env.SMTP_PORT || "587"),
-  secure: Number(process.env.SMTP_PORT) === 465,
+  host: process.env.SMTP_HOST,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
